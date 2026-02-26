2fb07e30977f800a8374ecb1a972f0458e6e4a75
Ingrid <ingridstanic@gmail.com>
2026-02-26 10:43:59 +0100

    added remove Btn to function for creating html

--- frontend/src/utils/createHTML.ts
+++ frontend/src/utils/createHTML.ts
@@ -12,15 +12,19 @@ export const createHtml = (products: Product[]) => {
     const title = document.createElement("h1");
     const description = document.createElement("p");
     const price = document.createElement("strong");
+    const removeBtn = document.createElement("button");
 
     container.className = "container";
     title.innerHTML = product.title;
     description.innerHTML = product.description;
-    price.innerHTML = product.price.toString();
+    price.innerHTML = product.price.toString() + " SEK";
+    removeBtn.className = "RemoveBtn";
+    removeBtn.innerHTML = "DELETE";
 
     container.appendChild(title);
     container.appendChild(description);
     container.appendChild(price);
+    container.appendChild(removeBtn);
 
     productContainer?.appendChild(container);
   });

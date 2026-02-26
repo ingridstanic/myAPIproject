559fc060260ce2eb20fee60661bb1489607b4a39
Ingrid <ingridstanic@gmail.com>
2026-02-26 21:13:41 +0100

    fixed duplicate ids

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -30,11 +30,11 @@ document.getElementById("sortBtn")?.addEventListener("click", async () => {
 
 document.getElementById("addProduct")?.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const title = (document.getElementById("title") as HTMLInputElement).value;
+  const title = (document.getElementById("addTitle") as HTMLInputElement).value;
   const description = (
-    document.getElementById("description") as HTMLInputElement
+    document.getElementById("addDescription") as HTMLInputElement
   ).value;
-  const price = (document.getElementById("price") as HTMLInputElement).value;
+  const price = (document.getElementById("addPrice") as HTMLInputElement).value;
 
   const newProduct: Product = {
     id: Date.now(),
@@ -46,4 +46,11 @@ document.getElementById("addProduct")?.addEventListener("submit", async (e) => {
   await addProduct(newProduct);
   const data = await getProducts();
   createHtml(data);
+
+  (e.target as HTMLFormElement).reset();
+
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: "smooth",
+  });
 });
